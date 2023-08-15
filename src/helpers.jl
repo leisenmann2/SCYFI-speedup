e996db4d36510b9ccbe0f1e74af108842b1ab14c
Lukas Eisenmann <lukas.eisenmann@zi-mannheim.de>
2023-08-15 10:42:52 +0200

    feat: added shPLRNN helpers

--- src/helpers.jl
+++ src/helpers.jl
@@ -26,6 +26,38 @@ function construct_relu_matrix_list(dim::Int, order::Integer)
 end
 
 """
+Construct a list of relu matrices for a random sequence of quadrants drawn from the allowed D's of the pool
+shPLRNN
+"""
+function construct_relu_matrix_list(relu_pool:: Array, order::Integer)
+    return relu_pool[:,:,rand(1:size(relu_pool)[3],order)]   
+end
+
+
+"""
+Initialise pool of admissable Relu matrices for the shallow PLRNN
+"""
+function construct_relu_matrix_pool(A:: Array, W1:: Array, W2:: Array, h1::Array, h2::Array,dim::Integer, hidden_dim::Integer)
+    # credit @Niclas Goering
+    n_points=10000000
+    corr=Matrix{Int64}(undef,n_points,hidden_dim)
+    #init array D_List to store all D in
+    m =rand(Uniform(-10,10),dim, n_points)
+    for j in 1:size(corr,1)
+        corr[j,:]=Bool.((W2 * m[:,j] .+ h2) .>0)
+    end
+    un=unique(corr,dims=1)
+    D_list=Array{Bool}(undef, hidden_dim, hidden_dim, size(un,1))
+    for k = 1:size(un,1)
+        D=Diagonal(Bool.(un[k,:]))
+        D_list[:,:,k] = D
+    end
+    return D_list
+end
+
+
+
+"""
 # Construct a list of relu matrices for a random sequence of quadrants faster version
 # """
 # function construct_relu_matrix_list(dim::Integer, order::Integer)
@@ -49,6 +81,31 @@ function get_cycle_point_candidate(A:: Array, W:: Array, D_list:: Array, h::Arra
     end
     return z_candidate #, z_factor
 end
+
+
+"""
+get the candidate for a cycle point by solving the cycle equation
+shPLRNN
+"""
+function get_cycle_point_candidate( A::AbstractVector,
+    W₁::AbstractMatrix,
+    W₂::AbstractMatrix,
+    h₁::AbstractVector,
+    h₂::AbstractVector,
+    D_list:: Array,
+    order::Integer)
+
+    z_factor, h₁_factor, h₂_factor = get_factors(A, W₁, W₂, D_list, order)
+    z_candidate = nothing
+    try
+        inverse_matrix = inv(I-z_factor)
+        z_candidate = inverse_matrix *(h₁_factor*h₁ + h₂_factor*h₂)
+    catch
+        #Not invertible?
+    end
+    return z_candidate #, z_factor
+end
+
 
 """
 recursively applying map gives us: (A+WD_k)*...*(A+WD_1) *z + [(A+WD_k)*...*(A+WD_2)+...+(A+WD)**1+1]*h = z
@@ -75,6 +132,26 @@ function get_factor_in_front_of_h(A:: Array, W:: Array, D_list:: Array, order::I
 end
 
 """
+Here we want to calculate the factors in front of z/h_1/h_2 recursively
+shPLRNN
+"""
+function get_factors(A::AbstractVector, W₁::AbstractMatrix, W₂::AbstractMatrix, D_list::Array, order::Integer)
+    hidden_dim = size(W₂)[1]
+    latent_dim = size(W₁)[1]
+    factor_z = I#Matrix(I,latent_dim,latent_dim)
+    factor_h₁ = I#Matrix(I,latent_dim,latent_dim)
+    factor_h₂ = W₁*D_list[:,:,1]*I#*Matrix(I,hidden_dim, hidden_dim)
+    for i = 1:order-1
+        factor_z = (Diagonal(A) + (W₁*D_list[:,:,i])*W₂)*factor_z
+        factor_h₁ = (Diagonal(A) + (W₁*D_list[:,:,i+1])*W₂)*factor_h₁ + I
+        factor_h₂ = (Diagonal(A) + (W₁*D_list[:,:,i+1])*W₂)*factor_h₂ + (W₁*D_list[:,:,i+1])
+    end
+    factor_z = (Diagonal(A) + (W₁*D_list[:,:,order])*W₂)*factor_z
+    return factor_z, factor_h₁, factor_h₂
+end
+
+
+"""
 Generate the time series by iteravely applying the PLRNN
 """
 function get_latent_time_series(time_steps:: Integer, A:: Array, W:: Array, h:: Array, dz::Integer; z_0:: Array= nothing)
@@ -93,11 +170,52 @@ function get_latent_time_series(time_steps:: Integer, A:: Array, W:: Array, h:: 
 end
 
 """
+Generate the time series by iteravely applying the shPLRNN
+shPLRNN
+"""
+function get_latent_time_series(time_steps:: Integer,     
+    A::AbstractVector,
+    W₁::AbstractMatrix,
+    W₂::AbstractMatrix,
+    h₁::AbstractVector,
+    h₂::AbstractVector,
+    dz::Integer;
+    z_0:: Array= nothing)
+    if z_0 === nothing
+        z = transpose(randn(1,dz))
+    else
+        z = z_0
+    end
+    trajectory = Array{Array}(undef, time_steps)
+    trajectory[1] = z
+    for t = 2:time_steps
+        z = latent_step(z, A, W₁, W₂, h₁, h₂)
+        trajectory[t] = z
+    end
+    return trajectory
+end
+
+
+"""
 PLRNN step
 """
 function latent_step(z:: Array, A:: Array, W:: Array, h::Array)
     return A*z + W*max.(0,z) + h
 end
+
+"""
+shPLRNN step
+"""
+function  latent_step(
+    z::AbstractArray,
+    A::AbstractVector,
+    W₁::AbstractMatrix,
+    W₂::AbstractMatrix,
+    h₁::AbstractVector,
+    h₂::AbstractVector,)
+    return A .* z .+ W₁ * max.(W₂ * z .+ h₂,0) .+ h₁
+end
+
 
 """
 Set the hyperparameters to predefined values if no value given (tuned for 2D case)
@@ -122,4 +240,22 @@ function get_eigvals(A:: Array, W:: Array, D_list:: Array, order::Integer)
     end
     return eigvals(e) 
 end
+
+
+"""
+Get the eigenvalues for all the points along the trajectory to learn about the stability
+shPLRNN
+"""
+function get_eigvals( A::AbstractVector,
+    W₁::AbstractMatrix,
+    W₂::AbstractMatrix, 
+    D_list:: Array, 
+    order::Integer)
+    e = I
+    for i = 1:order
+        e = (Diagonal(A) + (W₁ * D_list[:,:,i] * W₂)) * e
+    end
+    return eigvals(e) 
+end
+
 
