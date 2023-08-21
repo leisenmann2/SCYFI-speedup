e35e9a5691ffd0493cfe1679a5d954927189c758
Lukas Eisenmann <lukas.eisenmann@zi-mannheim.de>
2023-08-21 09:20:34 +0200

    feat: convPLRNN tests added

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,27 @@
 using SCYFI
 using Test
+include("./convPLRNN_tests.jl")
 
-@testset "SCYFI.jl" begin
-    # Write your tests here.
+@testset "SCYFI.jl" begin 
+    test_finding_1_cycle_2D_for_holes()
+    test_finding_1_cycle_2D()
+    test_finding_1_cycle_2D_val()
+    test_finding_16_cycle_2D()
+    test_finding_16_cycle_2D_nothing()
+    test_finding_27_cycle_2D()
+    test_finding_27_cycle_2D_nothing()
+    test_finding_31_cycle_2D()
+    test_finding_31_cycle_2D_nothing()
+    test_finding_40_cycle_2D()
+    test_finding_40_cycle_2D_nothing()
+    test_finding_53_cycle_2D()
+    test_finding_53_cycle_2D_nothing()
+    test_finding_65_cycle_2D()
+    test_finding_65_cycle_2D_nothing()
+    test_finding_80_cycle_2D()
+    test_finding_80_cycle_2D_nothing()
+    test_finding_83_cycle_2D()
+    test_finding_83_cycle_2D_nothing()
+    test_finding_10_cycle_10D()
+    test_finding_10_cycle_10D_nothing()
 end
