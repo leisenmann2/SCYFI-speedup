1cb7c35856f800fbeb831f95e0791804a9775732
Lukas Eisenmann <lukas.eisenmann@zi-mannheim.de>
2023-08-21 10:29:58 +0200

    feat: added shPLRNN tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,10 @@
 using SCYFI
 using Test
 include("./convPLRNN_tests.jl")
+include("./shPLRNN_tests.jl")
 
 @testset "SCYFI.jl" begin 
+    #conv PLRNN
     test_finding_1_cycle_2D_for_holes()
     test_finding_1_cycle_2D()
     test_finding_1_cycle_2D_val()
@@ -24,4 +26,10 @@ include("./convPLRNN_tests.jl")
     test_finding_83_cycle_2D_nothing()
     test_finding_10_cycle_10D()
     test_finding_10_cycle_10D_nothing()
+
+    #shPLRNN
+    test_finding_1_cycle_M2_H10()
+    test_finding_1_cycle_M2_H10_val()
+    test_finding_2_cycle_4_cycle_M2_H10()
+    test_finding_2_cycle_4_cycle_M2_H10_nothing()
 end
